6fbe65d439927dd970d7a25b26e9372a63cb99df
feigion@pdx.edu <feigion@pdx.edu>
2019-11-10 14:49:34 -0800

    assignment 5 is working

--- assignment5/app.js
+++ assignment5/app.js
@@ -1,14 +1,21 @@
-const http = require('http');
+const express = require('express');
 const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const server = express();
+const parser = require('body-parser');
+
+server.use(parser.urlencoded({'extended': false,'limit': 1024}))
+server.use('/', express.static(__dirname + '/public'));
+server.listen(8080);
+server.post('/submit', function(req, res) {
+  res.status(200);
+  res.write("Contact Name: " + req.body.contactName + '\n');
+
+  res.write("Contact Email: " + req.body.contactEmail + '\n');
+  res.write("Contact Comment: " + req.body.contactComment + '\n');
+  console.log(req.body);
+  res.end();
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
 
 
 
